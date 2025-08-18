c974b1ae430672dd2a24959ee89ab71c984738af
Megha <megha834004@gmail.com>
2025-08-18 23:40:23 +0530

    modified store.js

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,25 +1,50 @@
-import { configureStore } from "@reduxjs/toolkit" ;
-import rootReducer from "./reducers/rootReducer" ;
-import { applyMiddleware } from "@reduxjs/toolkit" ;
-import { thunk } from "redux-thunk" ;
-import { composeWithDevTools } from "redux-devtools-extension" ;
+// import { configureStore } from "@reduxjs/toolkit" ;
+// import rootReducer from "./reducers/rootReducer" ;
+// import { applyMiddleware } from "@reduxjs/toolkit" ;
+// import { thunk } from "redux-thunk" ;
+// import { composeWithDevTools } from "redux-devtools-extension" ;
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
+// const userInfoFromStorage = localStorage.getItem("userInfo")
+//   ? JSON.parse(localStorage.getItem("userInfo"))
+//   : null;
+
+// const initialState = {
+//   userLogin: { userInfo: userInfoFromStorage },
+// };
+
+// const store = configureStore(
+//   {
+//     reducer: rootReducer,
+//   },
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+// export default store ; 
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers/rootReducer";
+import { thunk } from "redux-thunk";
+  // default import, not { thunk }
+
+// Load user info from localStorage
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
-const initialState = {
+const preloadedState = {
   userLogin: { userInfo: userInfoFromStorage },
 };
 
-const store = configureStore(
-  {
-    reducer: rootReducer,
-  },
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(thunk), //  add thunk
+  preloadedState, // replaces your initialState
+  devTools: true, //  built-in Redux DevTools
+});
 
-export default store ; 
+export default store;
